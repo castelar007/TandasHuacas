cec4e118e796e44f0cb7c45dacd52a7c08e0fba7
castelar007 <castelar007@gmail.com>
2018-11-06 17:37:38 -0600

    home added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,15 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  sliderHeight;
+  list = [1,2,3,4,5,6,7,8 ,9 ,10];
   constructor(public navCtrl: NavController) {
 
   }
-
+  ionViewWillEnter(){
+    console.log('test');
+    this.sliderHeight = document.querySelector('.sliderContainer').clientHeight - 76;
+    console.log(this.sliderHeight);
+    
+  }
 }
