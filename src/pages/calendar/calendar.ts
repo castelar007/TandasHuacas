cda782fe10bc390d83ae900d85ca33e3856a6910
castelar007 <castelar007@gmail.com>
2018-11-08 17:07:06 -0600

    calendar added - no event label fixed

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {  NavController, NavParams } from 'ionic-angular';
-
+import * as moment from 'moment';
 @Component({
   selector: 'page-calendar',
   templateUrl: 'calendar.html',
