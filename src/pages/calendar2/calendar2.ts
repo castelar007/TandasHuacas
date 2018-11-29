ead1b2205cde63f594ada31b1ba1f7e5f25a918c
Ivan Castelar <i.castelar@beanar.io>
2018-11-29 17:26:18 -0600

    commit 11/29

--- src/pages/calendar2/calendar2.ts
+++ src/pages/calendar2/calendar2.ts
@@ -25,9 +25,17 @@ export class Calendar2Page {
     console.log('ionViewDidLoad Calendar2Page');
     this.options = {
       weekdays:['D', 'L', 'M', 'M', 'J', 'F', 'S'],
-      monthPickerFormat: ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DEC'],
+      // monthPickerFormat: ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SEP', 'OCT', 'NOV', 'DEC'],
+      showToggleButtons:false,
+      showMonthPicker:false,
+      // monthPickerFormat:['ENERO','FEBRERO','MARZO','ABRIL','MAYO','JUNIO','JULIO','AGOSTO','SEPTIEMBRE','OCTUBRE','NOVIEMBRE','DICIEMBRE'],
+      monthFormat:'MMMM YYYY',
       daysConfig:[
-        {date: new Date(2018, 11,10 ),marked:true}
+        {date: new Date(2018, 11,10 ),marked:true},
+        {date: new Date(2018, 11,11 ),marked:true},
+        {date: new Date(2018, 11,30 ),marked:true},
+        {date: new Date(2018, 11,1 ),marked:true},
+        {date: new Date(2018, 11,15 ),marked:true},
     ],
    
     }
